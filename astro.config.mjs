0331e7cfc8595334513fc73de930999c0dfcf2a2
VanillaMeow <leah.catgirlnyah@gmail.com>
2026-02-17 21:05:40 +0100

    guh

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://vanillameow.github.io',
+    base: '/personal-website',
     integrations: [svelte(), UnoCSS({ injectReset: true })],
 });
